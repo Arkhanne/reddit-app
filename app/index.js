f46a3fbabc9069e165f28d7d2cc48d032731e840
Edu Benito <eduardbenito@icloud.com>
2018-09-09 19:33:11 +0200

    add DetailsScreen and Details route

--- app/index.js
+++ app/index.js
@@ -14,9 +14,25 @@ class HomeScreen extends React.Component {
   }
 }
 
-const RootStack = createStackNavigator({
-  Home: HomeScreen
-});
+class DetailsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+      </View>
+    );
+  }
+}
+
+const RootStack = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Details: DetailsScreen,
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
 
 export default class App extends Component {
   render () {
