114b208c030a242095fc8fa00676bbc96fba75dd
Edu Benito <eduardbenito@icloud.com>
2018-09-09 19:23:35 +0200

    add StackNavigator and Home route

--- app/index.js
+++ app/index.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
+import { View } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
 
 import PostsList from './components/PostsList/PostsList';
+
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <PostsList></PostsList>
+      </View>
+    );
+  }
+}
+
+const RootStack = createStackNavigator({
+  Home: HomeScreen
+});
 
 export default class App extends Component {
   render () {
     return (
-      <PostsList></PostsList>
-    )
+      <RootStack />
+    );
   }
 }
