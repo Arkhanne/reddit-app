003a5a6eaf2667d1e7f90b41b6a121271ff129b3
Edu Benito <eduardbenito@icloud.com>
2018-09-14 11:08:21 +0200

    add basic detail component

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,10 +1,12 @@
 import { createStackNavigator } from 'react-navigation';
 
 import HomeScreen from '../screens/Home';
+import DetailScreen from '../screens/Detail';
 
 export default RootStack = createStackNavigator(
   {
     Home: HomeScreen,
+    Detail: DetailScreen,
   },
   {
     initialRouteName: 'Home',
