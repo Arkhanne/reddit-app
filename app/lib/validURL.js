3d9fed92783f5eccff873d28a74d31be3966e25b
Edu Benito <eduardbenito@icloud.com>
2018-09-16 11:05:04 +0200

    ES6 refactor var -> const

--- app/lib/validURL.js
+++ app/lib/validURL.js
@@ -1,6 +1,7 @@
 function validURL(str) {
-  var regex = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/;
-  if(!regex .test(str)) {
+  const regex = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/;
+  
+  if(!regex.test(str)) {
     return false;
   } else {
     return true;
